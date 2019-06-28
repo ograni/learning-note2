9ab451580f86ffd0483c004318f12d57862d2645
ograni <ograni@hanmail.net>
2019-06-28 14:00:54 +0900

    학습한거복습중

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    title: 'Vue.js 정복 캠프 학습 노트 복습',
+    description: '나는 오늘 무엇을 배웠는가',
+    base: '/learning-note2/',
+    // head: [
+    //   [
+    //     'link',
+    //     {
+    //       rel: 'icon',
+    //       href: '/logo.png',
+    //     },
+    //   ],
+    // ],
+    themeConfig: {
+        sidebar: [
+            '/day1',
+            {
+                title: 'Day 1입니다',
+                collapsable: false,
+                children: ['/components'],
+            },
+        ],
+    },
+};
